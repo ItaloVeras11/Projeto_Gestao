0fe52d4a3eb86fe0b9931b60eebde63d03874179
ItaloVeras11 <italocabralveras@gmail.com>
2023-02-20 16:37:15 -0300

    modified:   SuperGestao/resources/views/site/contato.blade.php 	modified:   SuperGestao/resources/views/site/principal.blade.php 	modified:   SuperGestao/resources/views/site/sobre-nos.blade.php 	modified:   SuperGestao/routes/web.php

--- SuperGestao/routes/web.php
+++ SuperGestao/routes/web.php
@@ -16,19 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function (){ return 'login';})->name('site.login');
 
-Route::get('/sobreNos', [SobreNosController::class, 'sobreNos']);
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function() { return 'clientes';});
+    Route::get('/fornecedores', function() { return 'fornecedores';});
+    Route::get('/produtos', function() { return 'produtos';});
+});
 
-Route::get('/contato', [ContatoController::class, 'contato']);
 
-Route::get('/contato/{nome}/{categoria_id}', 
-function(
-    string $nome = 'Desconhecido',
-    int $categoria_id = 1)
-    {
-    echo "Estamos aqui: $nome - $categoria_id";
-})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+
 
 
 

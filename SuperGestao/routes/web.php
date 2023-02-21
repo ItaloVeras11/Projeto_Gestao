f01b0145695057c82f5955a2a42a926a246dd991
ItaloVeras11 <italocabralveras@gmail.com>
2023-02-20 23:42:33 -0300

    modified:   SuperGestao/routes/web.php

--- SuperGestao/routes/web.php
+++ SuperGestao/routes/web.php
@@ -27,6 +27,14 @@ Route::prefix('/app')->group(function(){
     Route::get('/produtos', function() { return 'produtos';});
 });
 
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    echo 'Rota 2';
+})->name('site.rota2');
+
 
 
 

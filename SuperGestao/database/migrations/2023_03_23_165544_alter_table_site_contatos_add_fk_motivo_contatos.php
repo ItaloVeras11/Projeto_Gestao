eb1e45266ef82fb31cd4a11c46e6e832064d6159
ItaloVeras11 <italocabralveras@gmail.com>
2023-03-23 14:13:40 -0300

    modified:   SuperGestao/database/migrations/2023_03_23_165544_alter_table_site_contatos_add_fk_motivo_contatos.php

--- SuperGestao/database/migrations/2023_03_23_165544_alter_table_site_contatos_add_fk_motivo_contatos.php
+++ SuperGestao/database/migrations/2023_03_23_165544_alter_table_site_contatos_add_fk_motivo_contatos.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,7 +14,19 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        //Adicionando a coluna motivo_contatos_id
+        Schema::table('site_contatos', function (Blueprint $table) {
+            $table->unsignedBigInteger('motivo_contatos_id');
+        });
+
+        //Atribuindo motivo_contato para a nova coluna motivo_contatos_id
+        DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
+
+        //Criando FK e removendo a coluna motivo contato 
+        Schema::table('site_contatos', function (Blueprint $table) {
+            $table->foreign('motivo_contatos_id')->references('id')->on('motivo_contatos');
+            $table->dropColumn('motivo_contato');
+        });
     }
 
     /**

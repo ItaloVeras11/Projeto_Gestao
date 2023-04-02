75a34587db3b61ba5a6c832c19999ebade83b14c
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-01 22:10:49 -0300

    modified:   SuperGestao/routes/web.php

--- SuperGestao/routes/web.php
+++ SuperGestao/routes/web.php
@@ -24,6 +24,7 @@ Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
 

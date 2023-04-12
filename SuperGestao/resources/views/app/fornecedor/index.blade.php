c7d7cc5950fc1b6eeda654dce9563ee912b794d4
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-12 14:20:07 -0300

    modified:   SuperGestao/resources/views/app/fornecedor/index.blade.php

--- SuperGestao/resources/views/app/fornecedor/index.blade.php
+++ SuperGestao/resources/views/app/fornecedor/index.blade.php
@@ -5,19 +5,22 @@
 @section('conteudo')
     <div class="conteudo-pagina">
 
-        <div class="titulo-pagina">
+        <div class="titulo-pagina-2">
             <p>Fornecedor</p>
         </div>
 
         <div class="menu">
-
+            <ul>
+                <li><a href="">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
         </div>
 
         <div class="informacao-pagina">
 
         </div>
 
-        
+
 
     </div>
 @endsection

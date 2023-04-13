1be767ca781147fe8edb0b693a1ff83027152218
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-12 22:44:57 -0300

    new file:   SuperGestao/resources/views/app/fornecedor/adicionar.blade.php 	new file:   SuperGestao/resources/views/app/fornecedor/listar.blade.php

---
+++ SuperGestao/resources/views/app/fornecedor/listar.blade.php
@@ -0,0 +1,36 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Fornecedor')
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+
+        <div class="titulo-pagina-2">
+            <p>Fornecedor - Listar</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+
+            <div style="width: 30%; margin-left: auto; margin-right: auto;">
+                <form action="" method="post">
+                    <input type="text" name="nome" id="" placeholder="Nome" class="borda-preta">
+                    <input type="text" name="site" id="" placeholder="Site" class="borda-preta">
+                    <input type="text" name="uf" id="" placeholder="UF" class="borda-preta">
+                    <input type="text" name="email" id="" placeholder="E-mail" class="borda-preta">
+                    <button type="submit" class="borda-preta">Pesquisar</button>
+                </form>
+            </div>
+
+        </div>
+
+
+
+    </div>
+@endsection

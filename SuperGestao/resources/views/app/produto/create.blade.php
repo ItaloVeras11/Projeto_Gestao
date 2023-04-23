00eae7df765b7b9b3a3c9075e354e6e2ecb6c449
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-23 19:21:31 -0300

    modified:   SuperGestao/resources/views/app/produto/create.blade.php

--- SuperGestao/resources/views/app/produto/create.blade.php
+++ SuperGestao/resources/views/app/produto/create.blade.php
@@ -24,16 +24,16 @@
                     
                     @csrf
                     <input type="text" name="nome" value="{{$fornecedor->nome ?? old('nome') }}" id="" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : ''}}
+                    
 
                     <input type="text" name="site" value="{{ $fornecedor->site ??  old('site') }}" id="" placeholder="Site" class="borda-preta">
-                    {{ $errors->has('site') ? $errors->first('site') : ''}}
+                    
 
                     <input type="text" name="uf" value="{{ $fornecedor->uf ??  old('uf') }}" id="" placeholder="UF" class="borda-preta">
-                    {{ $errors->has('uf') ? $errors->first('uf') : ''}}
+                    
 
                     <input type="text" name="email" value="{{ $fornecedor->email ??  old('email') }}" id="" placeholder="E-mail" class="borda-preta">
-                    {{ $errors->has('email') ? $errors->first('email') : ''}}
+                    
 
                     <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>

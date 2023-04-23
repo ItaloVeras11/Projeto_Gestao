b5cab451112febb7daf402ef66048eb2aab25281
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-23 19:20:37 -0300

    modified:   SuperGestao/resources/views/app/produto/create.blade.php

--- SuperGestao/resources/views/app/produto/create.blade.php
+++ SuperGestao/resources/views/app/produto/create.blade.php
@@ -11,13 +11,13 @@
 
         <div class="menu">
             <ul>
-                <li><a href="{{ route('app.fornecedor.adicionar') }}">Novo</a></li>
-                <li><a href="{{ route('app.fornecedor') }}">Consulta</a></li>
+                <li><a href="">Novo</a></li>
+                <li><a href="">Consulta</a></li>
             </ul>
         </div>
 
         <div class="informacao-pagina">
-            {{ $msg ?? '' }}
+            
 
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
                 <form action="{{ route('app.fornecedor.adicionar')}}" method="post">

a82e3a9cbb21c87eb9d6f05968c2b9c8d327aca9
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-29 23:59:04 -0300

    modified:   SuperGestao/resources/views/app/produto/show.blade.php

--- SuperGestao/resources/views/app/produto/show.blade.php
+++ SuperGestao/resources/views/app/produto/show.blade.php
@@ -35,7 +35,7 @@
                     </tr>
                     <tr>
                         <td>Peso:</td>
-                        <td>{{ $produto->peso }}</td>
+                        <td>{{ $produto->peso }} kg</td>
                     </tr>
                     <tr>
                         <td>Unidade De Medida:</td>

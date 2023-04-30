c95ae7f16c253777817b4e27c55439ae2115df42
ItaloVeras11 <italocabralveras@gmail.com>
2023-04-30 00:26:26 -0300

    modified:   SuperGestao/resources/views/app/produto/edit.blade.php

--- SuperGestao/resources/views/app/produto/edit.blade.php
+++ SuperGestao/resources/views/app/produto/edit.blade.php
@@ -23,13 +23,13 @@
                 <form action="" method="post">
                     
                     @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" id="" placeholder="Nome" class="borda-preta">
+                    <input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" id="" placeholder="Nome" class="borda-preta">
                     {{ $errors->has('nome') ? $errors->first('nome') : '' }}
 
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" id="" placeholder="Descricao" class="borda-preta">
+                    <input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" id="" placeholder="Descricao" class="borda-preta">
                     {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
 
-                    <input type="text" name="peso" value="{{ old('peso') }}" id="" placeholder="Peso" class="borda-preta">
+                    <input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" id="" placeholder="Peso" class="borda-preta">
                     {{ $errors->has('peso') ? $errors->first('peso') : '' }}
 
                    <select name="unidade_id" >
@@ -37,7 +37,7 @@
                         <option>-- Selecione Unidade de Medida --</option>
 
                         @foreach ($unidades as $unidade)
-                        <option value="{{ $unidade->id }}"  {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}  > {{$unidade->descricao}} </option>
+                        <option value="{{ $unidade->id }}"  {{  old('unidade_id') == $unidade->id ? 'selected' : '' }}  > {{$unidade->descricao}} </option>
                         @endforeach
                         
 

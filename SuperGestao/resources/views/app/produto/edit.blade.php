1a6a99c12670df29e3e51e693da65c140e0d3eff
ItaloVeras11 <italocabralveras@gmail.com>
2023-05-03 15:04:59 -0300

    modified:   SuperGestao/resources/views/app/produto/edit.blade.php

--- SuperGestao/resources/views/app/produto/edit.blade.php
+++ SuperGestao/resources/views/app/produto/edit.blade.php
@@ -20,34 +20,9 @@
             
 
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form action="{{ route('produto.update', ['produto' = $produto->id]) }}" method="post">
+                @component('app.produto._components.form_create_edit')
                     
-                    @csrf
-                    @method('PUT')
-                    <input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" id="" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                    <input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" id="" placeholder="Descricao" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                    <input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" id="" placeholder="Peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                   <select name="unidade_id" >
-
-                        <option>-- Selecione Unidade de Medida --</option>
-
-                        @foreach ($unidades as $unidade)
-                        <option value="{{ $unidade->id }}"  {{ ($produto->unidade_id ?? old('unidade_id')) == $unidade->id ? 'selected' : '' }}  > {{$unidade->descricao}} </option>
-                        @endforeach
-                        
-
-                   </select>
-                   {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-                    
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @endcomponent
             </div>
 
         </div>

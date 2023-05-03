69c1018f144035518dafc54a5a4e6880620f3617
ItaloVeras11 <italocabralveras@gmail.com>
2023-05-03 15:07:31 -0300

    modified:   SuperGestao/resources/views/app/produto/create.blade.php

--- SuperGestao/resources/views/app/produto/create.blade.php
+++ SuperGestao/resources/views/app/produto/create.blade.php
@@ -6,11 +6,7 @@
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
-            @if (isset($produto->id))
-                <p>Editar Produto</p>
-            @else
-                <p>Adicionar Produto</p>
-            @endif
+            <p>Adicionar Produto</p>
         </div>
 
         <div class="menu">
@@ -22,8 +18,7 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                @component('app.produto._components.form_create_edit')
-                    
+                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
                 @endcomponent
             </div>
 

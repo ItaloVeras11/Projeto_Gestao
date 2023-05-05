0bb447042922dc009c46630e8276d69624663310
ItaloVeras11 <italocabralveras@gmail.com>
2023-05-05 14:54:03 -0300

    modified:   SuperGestao/resources/views/app/produto_detalhe/_components/form_create_edit.blade.php

--- SuperGestao/resources/views/app/produto_detalhe/_components/form_create_edit.blade.php
+++ SuperGestao/resources/views/app/produto_detalhe/_components/form_create_edit.blade.php
@@ -1,22 +1,22 @@
-@if (isset($produto->id))
-    <form action="{{ route('produto.update', ['produto' => $produto->id]) }}" method="post">
+@if (isset($produto_detalhe->id))
+    <form action="{{ route('produto.update', ['produto' => $produto_detalhe->id]) }}" method="post">
         @csrf
         @method('PUT')
-    @else
-        <form action="{{ route('produto.store') }}" method="post">
+@else
+    <form action="{{ route('produto-detalhe.store') }}" method="post">
 
-            @csrf
+        @csrf   
 @endif
-<input type="text" name="nome" value="{{ $produto->nome ?? old('nome') }}" id="" placeholder="Nome"
+<input type="text" name="nome" value="{{ $produto_detalhe->nome ?? old('nome') }}" id="" placeholder="Nome"
     class="borda-preta">
 {{ $errors->has('nome') ? $errors->first('nome') : '' }}
 
-<input type="text" name="descricao" value="{{ $produto->descricao ?? old('descricao') }}" id=""
+<input type="text" name="descricao" value="{{ $produto_detalhe->descricao ?? old('descricao') }}" id=""
     placeholder="Descricao" class="borda-preta">
 {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
 
-<input type="text" name="peso" value="{{ $produto->peso ?? old('peso') }}" id="" placeholder="Peso"
-    class="borda-preta">
+<input type="text" name="peso" value="{{ $produto_detalhe->peso ?? old('peso') }}" id=""
+    placeholder="Peso" class="borda-preta">
 {{ $errors->has('peso') ? $errors->first('peso') : '' }}
 
 <select name="unidade_id">
@@ -26,7 +26,7 @@
     @foreach ($unidades as $unidade)
         ??
         <option value="{{ $unidade->id }}"
-            {{ $produto->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '' }}>
+            {{ $produto_detalhe->unidade_id ?? old('unidade_id') == $unidade->id ? 'selected' : '' }}>
             {{ $unidade->descricao }} </option>
     @endforeach
 
